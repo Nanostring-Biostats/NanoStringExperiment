4b901aa4f1b7e9c66a7e8db715f59af000531bc6
NicoleEO <Nicole.Ortogero@gmail.com>
2022-03-30 09:42:19 -0700

    Delete correct method

--- R/NanoStringExperiment-accessors.R
+++ R/NanoStringExperiment-accessors.R
@@ -313,15 +313,30 @@ setMethod("varLabels", signature = "NanoStringExperiment",
 #' It is recommended to use the SummarizedExperiment method colData instead.
 #' This is a convenience method for backwards compatibility.
 #' 
+#' @importMethodsFrom Biobase varLabels
+#' 
+#' @export
+setMethod("varLabels", signature = "DataFrame",
+    function(object) colnames(object))
+
+#' Access sample phenotypic metadata variables
+#' 
+#' It is recommended to use the SummarizedExperiment method colData instead.
+#' This is a convenience method for backwards compatibility.
+#' 
 #' @importMethodsFrom Biobase varLabels<-
 #' 
 #' @export
-setReplaceMethod("varLabels", signature = "DataFrame",
+setReplaceMethod("varLabels", signature = "NanoStringExperiment",
     function(object, value) {
-        colnames(object) <- value
+        if(is.null(metadata(object)[["phenotypeCols"]])) {
+            colnames(colData(object)) <- value
+        } else {
+            colnames(colData(object)[, 
+                metadata(object)[["phenotypeCols"]]]) <- value
+        }
         return(object)
     })
-
 
 # Row Metadata (Formerly Feature Data) ----------------------------------------
 #fData
