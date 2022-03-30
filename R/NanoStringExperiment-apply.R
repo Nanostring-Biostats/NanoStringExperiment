efe92c4a79da5e928095484952299b80b7456ecc
NicoleEO <Nicole.Ortogero@gmail.com>
2022-03-30 15:26:21 -0700

    Applies

--- R/NanoStringExperiment-apply.R
+++ R/NanoStringExperiment-apply.R
@@ -1,5 +1,41 @@
 # Other 
-#assayDataApply
-#esApply
 #munge
+#summary
+#setQCFlags
+#esBy
+
+#' Apply across an assay
+#' 
+#' Apply function row (feature) or column (sample)-wise
+#' to selected assay
+#' 
+#' @export
+setGeneric("assayDataApply", signature = "X", 
+    function(X, MARGIN, FUN, ...) standardGeneric("assayDataApply"))
+
+#' Apply across an assay
+#' 
+#' Apply function row (feature) or column (sample)-wise
+#' to selected assay
+#' 
+#' @export
+setGeneric("assayDataApply", signature = "X", 
+    function(X, MARGIN, FUN, ..., elt = "exprs") {
+        exprsAssay <- assay(X, elt)
+        return(apply(exprsAssay, MARGIN, FUN, ...))
+    })
+
+#' Apply across default exprs assay
+#' 
+#' Apply function row (feature) or column (sample)-wise
+#' to the default "exprs" assay
+#' 
+#' @importMethodsFrom Biobase esApply
+#' 
+#' @export
+setMethod("esApply", signature = "NanoStringExperiment",
+    function(X, MARGIN, FUN, ...) {
+        exprsAssay <- assay(X, "exprs")
+        return(apply(exprsAssay, MARGIN, FUN, ...))
+    })
 
