8f7a6e60317469639c9c1f53646e5a4045119769
NicoleEO <Nicole.Ortogero@gmail.com>
2022-03-30 21:18:29 -0700

    Munge

---
+++ R/NanoStringExperiment-summary.R
@@ -0,0 +1,11 @@
+.summaryMetadata <- list(
+    log2 = data.frame(labelDescription = c("Geometric Mean", 
+        "Geometric Mean Size Factor", "Mean Log2", "SD Log2"), 
+        row.names = c("GeomMean", "SizeFactor", "MeanLog2", "SDLog2"), 
+        stringsAsFactors = FALSE), 
+    moments = data.frame(labelDescription = c("Mean", "Standard Deviation", 
+        "Skewness", "Excess Kurtosis"), row.names = c("Mean", "SD", 
+        "Skewness", "Kurtosis"), stringsAsFactors = FALSE), 
+    quantiles = data.frame(labelDescription = c("Minimum", "First Quartile", 
+        "Median", "Third Quartile", "Maximum"), row.names = c("Min", "Q1", 
+        "Median", "Q3", "Max"), stringsAsFactors = FALSE))
