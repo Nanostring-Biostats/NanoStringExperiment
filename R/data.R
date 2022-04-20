9c6ed26a4f9a8ab3b648f4b5fd21c9e3486681a5
NicoleEO <Nicole.Ortogero@gmail.com>
2022-04-20 02:11:34 -0700

    Add data doc

---
+++ R/data.R
@@ -0,0 +1,63 @@
+#' NanoString annotations
+#' 
+#' List of NanoString GeoMx annotations
+#' 
+#' @source NanoString Technologies
+#' @docType data
+#' @name "testAnnotation"
+
+#' NanoString expression data
+#' 
+#' Matrix of NanoString GeoMx expression data
+#' 
+#' @source NanoString Technologies
+#' @docType data
+#' @name "testAssayData"
+
+#' NanoString dimension labels
+#' 
+#' Vector with feature and sample metadata labels
+#' 
+#' @source NanoString Technologies
+#' @docType data
+#' @name "testDimLabels"
+
+#' Example NanoStringExperiment object
+#' 
+#' Resulting NanoStringExperiment object built from example data
+#' 
+#' @source NanoString Technologies
+#' @docType data
+#' @name "testExp"
+
+#' NanoString experiment data
+#' 
+#' MIAME experiment data for GeoMx dtaa
+#' 
+#' @source NanoString Technologies
+#' @docType data
+#' @name "testExperimentData"
+
+#' NanoString feature metadata
+#' 
+#' AnnotatedDataFrame of NanoString GeoMx feature metadata
+#' 
+#' @source NanoString Technologies
+#' @docType data
+#' @name "testFeatureData"
+
+#' NanoString sample phenotypic metadata
+#' 
+#' AnnotatedDataFrame of NanoString GeoMx phenotypic sample metadata
+#' 
+#' @source NanoString Technologies
+#' @docType data
+#' @name "testPhenoData"
+
+#' NanoString sample protocol metadata
+#' 
+#' AnnotatedDataFrame of NanoString GeoMx protocol sample metadata
+#' 
+#' @source NanoString Technologies
+#' @docType data
+#' @name "testProtocolData"
